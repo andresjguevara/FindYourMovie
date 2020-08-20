a0f2acb73664821d2da36be8cba5f590c65b2f33
Andres Guevara Caprio <Andres.GuevaraCaprio@virginpulse.com>
2020-08-20 13:23:51 -0400

    Created search controller that displays user's last searches

---
+++ FindYourMovie/Controllers/MovieSearchController.swift
@@ -0,0 +1,24 @@
+//
+//  MovieSearchController.swift
+//  FindYourMovie
+//
+//  Created by Andres Guevara Caprio on 8/19/20.
+//  Copyright © 2020 Andres Guevara Caprio. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
